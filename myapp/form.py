57fc2e71c9d543ce20f7d82cc7ea7d8d95318cf1
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-12 17:25:25 +0900

    commit: レシピ投稿画面のUIの実装

--- myapp/form.py
+++ myapp/form.py
@@ -3,14 +3,23 @@ from .models import Recipe,Contact
 
 #レシピ作成、編集用フォーム
 class RecipeForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'recipe_form'
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     for field_name, field in self.fields.items():
+    #         field.widget.attrs['class'] = 'recipe_form'
         
     class Meta:
         model = Recipe
         fields = ('title', 'image', 'items', 'steps', 'cook_time', 'created_date', 'category', 'tags')
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'タイトル', 'class': 'title_input'}),
+            'image': forms.FileInput(attrs={'class': 'image_input'}),
+            'steps': forms.Textarea(attrs={'class': 'steps_textarea'}),
+            'items': forms.Textarea(attrs={'class': 'items_textarea'}),
+            'cook_time': forms.NumberInput(attrs={'class': 'form-cook_time'}),
+            'category': forms.Select(attrs={'class': 'form-category'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-tags'}),
+        }
 
 
 
